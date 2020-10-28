0131fda04047598aea6800129fa811db66d03144
Korolev Vladimir <koroloff@list.ru>
2020-10-28 16:06:48 +0300

    (10.1 - 10.4] - Create conference Index & Detail pages

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,19 +2,51 @@
 
 namespace App\Controller;
 
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use App\Repository\ConferenceRepository;
+use Twig\Environment;
 
 class ConferenceController extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
+     *
+     * @param Environment          $twig
+     * @param ConferenceRepository $conferenceRepository
+     *
+     * @return Response
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
      */
-    public function index(): Response
+    public function index(Environment $twig, ConferenceRepository $conferenceRepository): Response
     {
-        return $this->render('conference/index.html.twig', [
-            'controller_name' => 'ConferenceController',
-        ]);
+        return new Response($twig->render('conference/index.html.twig', [
+            'conferences' => $conferenceRepository->findAll(),
+        ]));
+    }
+
+    /**
+     * @Route("/conference/{id}", name="conference")
+     *
+     * @param Environment       $twig
+     * @param Conference        $conference
+     * @param CommentRepository $commentRepository
+     *
+     * @return Response
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function show(Environment $twig, Conference $conference, CommentRepository $commentRepository): Response
+    {
+        return new Response($twig->render('conference/show.html.twig', [
+            'conference' => $conference,
+            'comments'   => $commentRepository->findBy(['conference' => $conference], ['createdAt' => 'DESC']),
+        ]));
     }
 }
--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -124,4 +124,12 @@ class Comment
 
         return $this;
     }
+
+    /**
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return (string) $this->getEmail();
+    }
 }
